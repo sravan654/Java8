98107244f03cb6dcaa56a5058080c53e288c6b58
Apuri, Sravan <Sravan.Apuri@cdk.com>
2018-01-21 19:41:12 +0530

    Lambda expression part2

--- src/com/lambdaexpressions/Main.java
+++ src/com/lambdaexpressions/Main.java
@@ -2,7 +2,6 @@ package com.lambdaexpressions;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -39,17 +38,38 @@ public class Main {
         employeeList.add(employee2);
         employeeList.add(employee);
 
-        Collections.sort(employeeList, new Comparator<Employee>() {
-            @Override
-            public int compare(Employee employee1, Employee employee2) {
-                return employee1.getName().compareTo(employee2.getName());
-            }
-        });
+//        Collections.sort(employeeList, new Comparator<Employee>() {
+//            @Override
+//            public int compare(Employee employee1, Employee employee2) {
+//                return employee1.getName().compareTo(employee2.getName());
+//            }
+//        });
+
+        //Sorting employees using lambda expression
+        Collections.sort(employeeList, (e1, e2)->
+        e1.getName().compareTo(e2.getName()));
 
         for (Employee e : employeeList){
             System.out.println(e.getName());
         }
 
+//        String s = doStringStuff(new UpperConcat() {
+//            @Override
+//            public String upperAndConcat(String s1, String s2) {
+//                return s1.toUpperCase() + s2.toUpperCase();
+//            }
+//        }, employeeList.get(0).getName(), employeeList.get(1).getName());
+
+
+        //Using lambda
+        UpperConcat uc = (s1, s2) -> s1.toUpperCase()+s2.toUpperCase();
+        String s = doStringStuff(uc,employeeList.get(0).getName(), employeeList.get(1).getName());
+        System.out.println(s);
+
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2){
+        return uc.upperAndConcat(s1,s2);
     }
 }
 
@@ -85,4 +105,8 @@ class Employee{
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat{
+    public String upperAndConcat(String s1, String s2);
 }
